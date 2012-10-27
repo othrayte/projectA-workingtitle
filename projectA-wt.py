bc86366cc96a12da91b91566adc659cf0b84779c
Adrian Cowan (othrayte) <othrayte@gmail.com>
2012-10-27 16:25:31 +1100

    Initial project setup

---
+++ projectA-wt.py
@@ -0,0 +1,27 @@
+
+"""
+    Projcet A (Working title)
+
+    Website     : github.com/othrayte/projectA-workingtitle
+    Initial idea: Adrian Cowan (othrayte) - othrayte@gmail.com
+    Project Contributors:
+        Adrian Cowan (othrayte) - othrayte@gmail.com
+
+    Copyright 2012, Adrian Cowan (othrayte) & listed contributors
+
+    This file is part of ProjectA.
+
+    ProjectA is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ProjectAis distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ProjectA.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
